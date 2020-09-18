da41b5b6f399edd9a4cbc9bee6d3f4b676b39a61
Dev <devmittal0407@gmail.com>
2020-09-18 19:47:07 +0530

    Our Successful Integration of Sign Up Module

--- authentication/views.py
+++ authentication/views.py
@@ -54,7 +54,7 @@ def CreateUser(request):
     if(serializer.is_valid()):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
 # Update Registered User Details API
 @api_view(['PUT'])
